316d7b8a36a0c544571757d7782b735e7ea388a3
dveltauri <diana.veltauri1@gmail.com>
2022-08-23 13:25:45 -0700

    Init commit

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int temp ;
+        int prev = 0;
+        int curr = 1;
+
+        if(lastFibonacci != 0){
+            System.out.println(prev);
+            for (int i=1; i<lastFibonacci; i++){
+                System.out.println(curr);
+                temp = curr;
+                curr = prev+curr;
+                prev = temp;
+            }
+        }
     }
 }
